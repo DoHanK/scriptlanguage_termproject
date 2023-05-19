c510d2099a500738dc6c09214d8017e6ab8ac844
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2023-05-19 14:53:53 +0900

    도리짓고 땡

---
+++ 도리짓고 땡/Player.py
@@ -0,0 +1,40 @@
+class Player: #클래스 Player (플레이어와 딜러 객체 만들때 사용)
+
+    def __init__(self,name):
+        self.name = name
+        self.cards = []     #Card 클래스 객체를 갖는 리스트
+        self.N = 0  #현재 갖고 있는 카드 개수
+
+    def inHand(self):#손에 쥐고 있는 카드 개수
+        return self.N
+
+    def addCard(self,c): #인자 c: Card 클래스 객체
+        self.cards.append(c)
+        self.N+=1
+
+    def reset(self):
+        self.N = 0
+        self.cards.clear() #점수 초기화
+
+    def value(self): # 점수를 계산해서 반환
+        sum = 0
+        onepair = []
+        for x in self.cards:
+            if x.value != 1:
+                if x.value > 9:
+                    sum += 10
+                else:
+                    sum += x.value
+            else:
+                onepair.append(x.value)
+        if onepair is not None:
+            for x in onepair:
+                if sum+11 > 21:
+                    sum += 1
+                else:
+                    sum += 11
+        return sum
+    #우선 ace =11로 계산 2~10숫나는 그대로 합산, jqk는 10으로 계싼
+    # 카드들의 점수를 합산하면서 ACE 카드 개수를 샌다
+    #합산 결과가 21이 넘어가면 ACE 하나씩 1로 변경해서 21이 안넘을때까지
+
---
+++ 도리짓고 땡/도리짓고 땡.py
@@ -0,0 +1,312 @@
+from tkinter import *
+from tkinter import font
+from winsound import *
+from Card import *
+from Player import*
+import random
+
+class Dori:
+    def hitPlayer1(self,n):
+        newCard = Card(self.cardDeck[self.deckN])
+        self.deckN += 1
+        self.player1.addCard(newCard)
+        p = PhotoImage(file='cards/'+newCard.filename())
+        self.LcardsPlayer.append(Label(self.window, image=p))
+        self.LcardsPlayer[self.player.inHand()-1].image = p
+        self.LcardsPlayer[self.player.inHand()-1].place(x=250+n*30, y=350)
+        self.LplayerPts.configure(text=str(self.player.value()))
+        PlaySound('sounds/cardFilp1.wav',SND_FILENAME)
+    def hitPlayer2(self,n):
+        newCard = Card(self.cardDeck[self.deckN])
+        self.deckN += 1
+        self.player2.addCard(newCard)
+        p = PhotoImage(file='cards/'+newCard.filename())
+        self.LcardsPlayer.append(Label(self.window, image=p))
+        self.LcardsPlayer[self.player.inHand()-1].image = p
+        self.LcardsPlayer[self.player.inHand()-1].place(x=250+n*30, y=350)
+        self.LplayerPts.configure(text=str(self.player.value()))
+        PlaySound('sounds/cardFilp1.wav',SND_FILENAME)
+    def hitPlayer3(self,n):
+        newCard = Card(self.cardDeck[self.deckN])
+        self.deckN += 1
+        self.player3.addCard(newCard)
+        p = PhotoImage(file='cards/'+newCard.filename())
+        self.LcardsPlayer.append(Label(self.window, image=p))
+        self.LcardsPlayer[self.player.inHand()-1].image = p
+        self.LcardsPlayer[self.player.inHand()-1].place(x=250+n*30, y=350)
+        self.LplayerPts.configure(text=str(self.player.value()))
+        PlaySound('sounds/cardFilp1.wav',SND_FILENAME)
+
+    def hitDealerDown(self):
+        newCard = Card(self.cardDeck[self.deckN])
+        self.deckN += 1
+        self.dealer.addCard(newCard)
+        p = PhotoImage(file='cards/b2fv.png')
+        self.LcardsDealer.append(Label(self.window, image=p))
+        self.LcardsDealer[self.dealer.inHand() - 1].image = p
+        self.LcardsDealer[self.dealer.inHand() - 1].place(x=250, y=150)
+        self.LdealerPts.configure(text=str(self.dealer.value()))
+        PlaySound('sounds/cardFilp1.wav', SND_FILENAME)
+    def hitDealer(self,n):
+        newCard = Card(self.cardDeck[self.deckN])
+        self.deckN += 1
+        self.dealer.addCard(newCard)
+        p = PhotoImage(file='cards/'+newCard.filename())
+        self.LcardsDealer.append(Label(self.window, image=p))
+        self.LcardsDealer[self.dealer.inHand() - 1].image = p
+        self.LcardsDealer[self.dealer.inHand() - 1].place(x=250 + n * 30, y=150)
+        self.LdealerPts.configure(text=str(self.dealer.value()))
+        PlaySound('sounds/cardFilp1.wav', SND_FILENAME)
+    def deal(self):
+        if self.cards == 0:
+            random.shuffle(self.cardDeck)
+            self.hitPlayer1(0)
+            self.hitPlayer2(0)
+            self.hitPlayer3(0)
+            self.hitDealer(0)
+            self.cards = 1
+        elif self.cards == 1:
+            self.hitPlayer1(1)
+            self.hitPlayer1(2)
+            self.hitPlayer1(3)
+            self.hitPlayer2(1)
+            self.hitPlayer2(2)
+            self.hitPlayer2(3)
+            self.hitPlayer3(1)
+            self.hitPlayer3(2)
+            self.hitPlayer3(3)
+            self.hitDealer(1)
+            self.hitDealer(2)
+            self.hitDealer(3)
+            self.cards = 4
+        else:
+            self.hitPlayer1(4)
+            self.hitPlayer2(4)
+            self.hitPlayer3(4)
+            self.hitDealer(4)
+            self.cards = 5
+        if self.cards == 5:
+
+
+
+    def pressedP1B5(self):
+        pass
+    def pressedP1B1(self):
+        pass
+    def pressedP2B5(self):
+        pass
+    def pressedP2B1(self):
+        pass
+    def pressedP3B5(self):
+        pass
+    def pressedP3B1(self):
+        pass
+    def pressedDeal(self):
+        self.deal()
+        self.P1B5['state'] = 'active'
+        self.P1B5['bg'] = 'white'
+        self.P1B1['state'] = 'active'
+        self.P1B1['bg'] = 'white'
+        self.P2B5['state'] = 'active'
+        self.P2B5['bg'] = 'white'
+        self.P2B1['state'] = 'active'
+        self.P2B1['bg'] = 'white'
+        self.P3B5['state'] = 'active'
+        self.P3B5['bg'] = 'white'
+        self.P3B1['state'] = 'active'
+        self.P3B1['bg'] = 'white'
+        self.Deal['state'] = 'disabled'
+        self.Deal['bg'] = 'gray'
+    def pressedAgain(self):
+        self.player1.reset()
+        self.player2.reset()
+        self.player3.reset()
+        self.dealer.reset()
+        self.P1B5['state'] = 'disabled'
+        self.P1B5['bg'] = 'gray'
+        self.P1B1['state'] = 'disabled'
+        self.P1B1['bg'] = 'gray'
+        self.P2B5['state'] = 'disabled'
+        self.P2B5['bg'] = 'gray'
+        self.P2B1['state'] = 'disabled'
+        self.P2B1['bg'] = 'gray'
+        self.P3B5['state'] = 'disabled'
+        self.P3B5['bg'] = 'gray'
+        self.P3B1['state'] = 'disabled'
+        self.P3B1['bg'] = 'gray'
+        self.Again['state'] = 'disabled'
+        self.Again['bg'] = 'gray'
+        self.Deal['state'] = 'active'
+        self.Deal['bg'] = 'white'
+
+        for x in self.LcardPlayer1:
+            x.destroy()
+        for x in self.LcardPlayer2:
+            x.destroy()
+        for x in self.LcardPlayer3:
+            x.destroy()
+        for x in self.LcardDealer:
+            x.destroy()
+        self.P1Lstatus.configure(text="")
+        self.P2Lstatus.configure(text="")
+        self.P3Lstatus.configure(text="")
+
+        self.LcardPlayer = []
+        self.LcardDealer = []
+        self.nCardsPlayer = 0 #플레이어 카드 위치변수
+        self.nCardsDealer = 0 #딜러 카드 위치 변수
+        self.betMoney = 0
+        self.player1.reset()
+        self.player2.reset()
+        self.player3.reset()
+        self.dealer.reset()
+        self.LbetMoney.configure(text='$' + str(self.betMoney))
+        self.LplayerMoney.configure(text='You have $' + str(self.playerMoney))
+        self.deckN = 0
+
+    def setupButton(self):
+        self.P1B5 =Button(self.window,text='Bet 50',width=10,height=2,font =self.fontstyle2,command=self.pressedP1B5)
+        self.P1B5.place(x=100,y=950)
+        self.P1B1 =Button(self.window,text='Bet 10',width=10,height=2,font =self.fontstyle2,command=self.pressedP1B1)
+        self.P1B1.place(x=250,y=950)
+        self.P2B5 =Button(self.window,text='Bet 1',width=10,height=2,font =self.fontstyle2,command=self.pressedP2B5)
+        self.P2B5.place(x=500,y=950)
+        self.P2B1 = Button(self.window, text='Bet 50', width=10, height=2, font=self.fontstyle2, command=self.pressedP2B1)
+        self.P2B1.place(x=650, y=950)
+        self.P3B5 = Button(self.window, text='Bet 10', width=10, height=2, font=self.fontstyle2, command=self.pressedP3B5)
+        self.P3B5.place(x=900, y=950)
+        self.P3B1 = Button(self.window, text='Bet 1', width=10, height=2, font=self.fontstyle2, command=self.pressedP3B1)
+        self.P3B1.place(x=1050, y=950)
+        self.Deal =Button(self.window,text='Deal',width=10,height=2,font =self.fontstyle2,command=self.pressedDeal)
+        self.Deal.place(x=1250,y=950)
+        self.Again =Button(self.window,text='Again',width=10,height=2,font =self.fontstyle2,command=self.pressedAgain)
+        self.Again.place(x=1400,y=950)
+        self.P1B5['state'] = 'disabled'
+        self.P1B5['bg'] = 'gray'
+        self.P1B1['state'] = 'disabled'
+        self.P1B1['bg'] = 'gray'
+        self.P2B5['state'] = 'disabled'
+        self.P2B5['bg'] = 'gray'
+        self.P2B1['state'] = 'disabled'
+        self.P2B1['bg'] = 'gray'
+        self.P3B5['state'] = 'disabled'
+        self.P3B5['bg'] = 'gray'
+        self.P3B1['state'] = 'disabled'
+        self.P3B1['bg'] = 'gray'
+        self.Again['state'] = 'disabled'
+        self.Again['bg'] = 'gray'
+
+    def setupLabel(self):
+        self.P1betMoney = Label(text='0만',width=7,height=1,font=self.fontstyle,bg='#2B652E',fg='yellow')
+        self.P1betMoney.place(x=200,y=900)
+
+        self.P2betMoney = Label(text='0만', width=7, height=1, font=self.fontstyle, bg='#2B652E', fg='yellow')
+        self.P2betMoney.place(x=600, y=900)
+
+        self.P3betMoney = Label(text='0만', width=7, height=1, font=self.fontstyle, bg='#2B652E', fg='yellow')
+        self.P3betMoney.place(x=1000, y=900)
+
+        self.LeftMoney = Label(text='1000만', width=7, height=1, font=self.fontstyle, bg='#205928', fg='yellow')
+        self.LeftMoney.place(x=1400, y=900)
+
+        self.P1card1 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P1card1.place(x=100, y=800)
+        self.P1card2 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P1card2.place(x=500, y=300)
+        self.P1card3 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P1card3.place(x=500, y=300)
+        self.P1card4 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P1card4.place(x=500, y=300)
+        self.P1card5 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P1card5.place(x=500, y=300)
+
+        self.P2card1 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P2card1.place(x=500, y=300)
+        self.P2card2 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P2card2.place(x=500, y=300)
+        self.P2card3 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P2card3.place(x=500, y=300)
+        self.P2card4 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P2card4.place(x=500, y=300)
+        self.P2card5 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P2card5.place(x=500, y=300)
+
+        self.P3card1 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P3card1.place(x=500, y=300)
+        self.P3card2 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P3card2.place(x=500, y=300)
+        self.P3card3 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P3card3.place(x=500, y=300)
+        self.P3card4 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P3card4.place(x=500, y=300)
+        self.P3card5 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P3card5.place(x=500, y=300)
+
+        self.Dcard1 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.Dcard1.place(x=500, y=300)
+        self.Dcard2 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.Dcard2.place(x=500, y=300)
+        self.Dcard3 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.Dcard3.place(x=500, y=300)
+        self.Dcard4 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.Dcard4.place(x=500, y=300)
+        self.Dcard5 = Label(text='', width=1, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.Dcard5.place(x=500, y=300)
+
+        self.Lplayer1Pts = Label(text='', width=6, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.Lplayer1Pts.place(x=300, y=300)
+
+        self.Lplayer2Pts = Label(text='', width=6, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.Lplayer2Pts.place(x=300, y=300)
+
+        self.Lplayer3Pts = Label(text='', width=6, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.Lplayer3Pts.place(x=300, y=300)
+
+        self.LDealerPts = Label(text='', width=6, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.LDealerPts.place(x=300, y=300)
+
+        self.P1Lstatus = Label(text='', width=15, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P1Lstatus.place(x=500, y=300)
+
+        self.P2Lstatus = Label(text='', width=15, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P2Lstatus.place(x=500, y=300)
+
+        self.P3Lstatus = Label(text='', width=15, height=1, font=self.fontstyle, bg='green', fg='white')
+        self.P3Lstatus.place(x=500, y=300)
+
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('도리짓고 땡')
+        self.window.geometry('1625x1081')
+        p = PhotoImage(file='GodoriCards/table.gif')
+        label = Label(self.window,image=p)
+        label.place(x=-2,y=-2)
+        self.fontstyle = font.Font(self.window, size=24, weight='bold',family='굴림체')
+        self.fontstyle2 = font.Font(self.window, size=16, weight='bold',family='굴림체')
+        self.setupButton()
+        self.setupLabel()
+
+        self.cards = 0
+        self.player1 = Player('player1')
+        self.player2 = Player('player2')
+        self.player3 = Player('player3')
+        self.dealer = Player('dealer')
+        self.P1betMoney = 0 #배팅 머니 변수
+        self.P2betMoney = 0 #배팅 머니 변수
+        self.P3betMoney = 0 #배팅 머니 변수
+        self.playerMoney = 1000 #플레이어가 갖고 있는 머니 변수
+        self.nCardsPlayer1 = 0 #플레이어 카드 위치변수
+        self.nCardsPlayer2 = 0 #플레이어 카드 위치변수
+        self.nCardsPlayer3 = 0 #플레이어 카드 위치변수
+        self.nCardsDealer = 0 #딜러 카드 위치 변수
+        self.LcardPlayer1 = [] #플레이어 카드 이미지 라벨들을 갖는 리스트
+        self.LcardPlayer2 = [] #플레이어 카드 이미지 라벨들을 갖는 리스트
+        self.LcardPlayer3 = [] #플레이어 카드 이미지 라벨들을 갖는 리스트
+        self.LcardDealer = []
+        self.cardDeck =[i for i in range(40)] #[0~51]
+        self.deckN = 0 #카드덱에서 몇번째 숫자를 선택하느냐 나타내는 변수
+        self.window.mainloop()
+
+
+Dori()
